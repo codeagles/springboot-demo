9465342c1e28be1e6eb91d9f035c6ddb39085282
houchunan <hcn0521@outlook.com>
2024-06-19 20:32:36 +0800

    初始化仓库

---
+++ src/main/java/com/codeagles/springbooteventdemo/SpringbootEventDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codeagles.springbooteventdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEventDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEventDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeagles/springbooteventdemo/SpringbootEventDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeagles.springbooteventdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEventDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
